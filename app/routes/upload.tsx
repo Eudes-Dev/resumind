2afde81bb7783be80134c638b019b57900e2a341
Eudes-Dev <euudesdev@gmail.com>
2025-08-03 21:26:27 +0000

    🚧 WIP: Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -76,6 +76,8 @@ const Upload = () => {
     await kv.set(`resume:${uuid}`, JSON.stringify(data))
     setStatusText('Analysis complete, redirecting...')
     console.log(data)
+
+    navigate(`/resume/${uuid}`)
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
